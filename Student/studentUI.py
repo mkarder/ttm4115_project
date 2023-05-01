205a102e9b5f51c24c857d6abf94eec347a0f3d9
HeleneAmlie <helene@amlie.org>
2023-05-01 14:12:59 +0200

    complete student?

--- Student/studentUI.py
+++ Student/studentUI.py
@@ -25,6 +25,7 @@ class StudentUI():
     def start_irat(self):
         self.app.showSubWindow("conduct_irat")
         self.app.setMessage("irat_name", self.rat["name"])
+        self.app.setLabel("irat_q_nr", "1")
         self.app.setMessage("irat_q", self.rat["questions"]["1"]["question"])
         alt = ["correct", "a", "b", "c"]
         random.shuffle(alt)
@@ -38,6 +39,39 @@ class StudentUI():
             self.app.hideButton("irat_next")
             self.app.showButton("irat_send")
 
+    def start_trat(self):
+        self.app.showSubWindow("conduct_trat")
+        self.app.setMessage("trat_name", self.rat["name"])
+        self.app.setLabel("trat_q_nr", "1")
+        self.app.setMessage("trat_q", self.rat["questions"]["1"]["question"])
+        alt = ["correct", "a", "b", "c"]
+        random.shuffle(alt)
+        i = 1
+        for alternative in alt:
+            self.app.setMessage(
+                "trat_" + str(i), self.rat["questions"]["1"][alternative])
+            i += 1
+
+    def trat_not_leader(self):
+        self.app.setLabel(
+            "info", "You are not the leader, see the leaders device to take tRAT")
+
+    def irat_done(self):
+        self.app.hideSubWindow("conduct_irat")
+        self.app.setLabel("info", "Waiting for your teammates to finish")
+
+    def open_login(self):
+        self.app.showSubWindow("sign_in")
+
+    def trat_done(self):
+        self.app.hideSubWindow("conduct_trat")
+        self.app.removeAllWidgets()
+        self.app.addLabel("welcome", "Welcome")
+        self.app.addButton("Sign in", self.open_login)
+        self.app.entry("Id", "")
+        self.app.entry("Name", "")
+        self.app.entry("Team", "")
+
     def create_ui(self):
 
         def sign_in():
@@ -48,14 +82,11 @@ class StudentUI():
                             "name": name, "team": team, "subject": "TTM4115"}
             self.app.hideSubWindow("sign_in")
             self.app.removeAllWidgets()
-            self.app.addLabel("info", "Waiting")
-            self.stm_driver.send("sign_in", "student")
-
-        def open_login():
-            self.app.showSubWindow("sign_in")
+            self.app.addLabel("info", "Waiting for a RAT")
+            self.stm.send("sign_in", "student")
 
         def cancel():
-            self.stm_driver.send("cancel", "student")
+            self.stm.send("cancel", "student")
             self.app.hideSubWindow("sign_in")
 
         def next_irat_question():
@@ -87,15 +118,51 @@ class StudentUI():
                 "irat_" + self.app.getRadioButton("irat_answer"))
             self.answers.append(check_answer(
                 self.rat["questions"][self.app.getLabel("irat_q_nr")], answer))
+            self.stm.send("irat_done", "student")
 
             self.app.hideSubWindow("conduct_irat")
             self.app.setLabel("info", "Waiting for your teammates to finish")
 
+        def check():
+            answer = self.app.getMessage(
+                "trat_" + self.app.getRadioButton("trat_answer"))
+            alt = check_answer(
+                self.rat["questions"][self.app.getLabel("trat_q_nr")], answer)
+            q_num = int(self.app.getLabel("trat_q_nr")) + 1
+            if len(self.answers) == int(self.app.getLabel("trat_q_nr")):
+                self.answers.append(alt)
+            if alt == "correct":
+                if str(q_num) not in self.rat["questions"]:
+                    self.app.showButton("trat_send")
+                else:
+                    self.app.showButton("trat_next")
+                self.app.hideButton("Check")
+                self.app.showSubWindow("correct")
+            else:
+                self.app.showSubWindow("incorrect")
+
         def next_trat_question():
-            pass
+            q_num = int(self.app.getLabel("trat_q_nr")) + 1
+
+            self.app.hideButton("trat_next")
+            self.app.showButton("Check")
+
+            self.app.setLabel("trat_q_nr", str(q_num))
+            self.app.setMessage(
+                "trat_q", self.rat["questions"][str(q_num)]["question"])
+
+            alt = ["correct", "a", "b", "c"]
+            random.shuffle(alt)
+            i = 1
+            for alternative in alt:
+                self.app.setMessage(
+                    "trat_" + str(i), self.rat["questions"][str(q_num)][alternative])
+                i += 1
 
         def send_trat_answers():
-            pass
+            self.stm.send("trat_done", "student")
+
+            self.app.hideSubWindow("conduct_trat")
 
         # Here comes the GUI
         self.app = gui("Student", "500x300")
@@ -104,7 +171,7 @@ class StudentUI():
         self.app.setFont(14)
 
         self.app.addLabel("welcome", "Welcome")
-        self.app.addButton("Login", open_login)
+        self.app.addButton("Sign in", self.open_login)
 
         # this is a subwindow for login
         self.app.startSubWindow("sign_in", "Login", modal=True)
@@ -140,7 +207,8 @@ class StudentUI():
 
         self.app.addMessage("trat_name", "", colspan=6)
         self.app.setMessageWidth("trat_name", 650)
-        self.app.addMessage("trat_q", "", colspan=6)
+        self.app.addLabel("trat_q_nr", "1", 1, 0, 1)
+        self.app.addMessage("trat_q", "", 1, 1, 5)
         self.app.setMessageWidth("trat_q", 650)
 
         self.app.addRadioButton("trat_answer", "1", 3, 0, colspan=1)
@@ -158,7 +226,7 @@ class StudentUI():
 
         self.app.addButton("trat_send", send_trat_answers, colspan=6)
         self.app.setButton("trat_send", "Send")
-        self.app.addButton("Check", check_answer, colspan=6)
+        self.app.addButton("Check", check, colspan=6)
         self.app.addButton("trat_next", next_trat_question, colspan=6)
         self.app.setButton("trat_next", "Next question")
         self.app.hideButton("trat_send")
